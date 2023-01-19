3c28a34917085dbe97545a8ac9a83f4b95f5703c
cwbhhjl <cwbhhjl@gmail.com>
2023-01-19 14:25:02 +0800

    update run sh

--- run.sh
+++ run.sh
@@ -3,11 +3,13 @@ docker-compose up -d zkevm-state-db
 sleep 1
 docker-compose up -d zkevm-prover
 sleep 3
-docker-compose up -d zkevm-sequencer
 docker-compose up -d zkevm-aggregator
-docker-compose up -d zkevm-json-rpc
-docker-compose up -d zkevm-sync
+docker-compose up -d zkevm-sequencer
 docker-compose up -d zkevm-broadcast
+
+docker-compose up -d zkevm-sync
+docker-compose up -d zkevm-json-rpc
+
 docker-compose up -d zkevm-bridge-service
 docker-compose up -d zkevm-bridge-ui
 docker-compose up -d zkevm-explorer-json-rpc
