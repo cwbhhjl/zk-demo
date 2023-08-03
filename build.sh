3b3047f8330bd14bee025655634cae3de96f661e
jianguo <zjg555543@163.com>
2023-08-03 10:54:53 +0800

    update

--- build.sh
+++ build.sh
@@ -1,14 +1,17 @@
-cd ../zkevm-git/zkevm-node
-make build-docker
+cd ../
+git clone -b release/v0.2.1 https://github.com/0xPolygonHermez/zkevm-node.git
+git clone -b release/v0.2.0 https://github.com/0xPolygonHermez/zkevm-bridge-service.git
+git clone -b develop https://github.com/0xPolygonHermez/zkevm-bridge-ui.git
 
+cd ./zkevm-node
+make build-docker
 cd -
 
-cd ../zkevm-git/zkevm-bridge-service
+cd ./zkevm-bridge-service
 make build-docker
-
 cd -
 
-cd ../zkevm-git/zkevm-bridge-ui
+cd ./zkevm-bridge-ui
 docker build . -t zkevm-bridge-ui:local
-
 cd -
+cd zk-demo
