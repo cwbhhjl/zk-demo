395bda0c5dec155aceb8f2d73eb76df7b40e501b
jianguo <zjg555543@163.com>
2023-08-03 10:11:50 +0800

    update

--- build.sh
+++ build.sh
@@ -1,10 +1,14 @@
-cd ./zkevm-node
+cd ../zkevm-git/zkevm-node
 make build-docker
 
-cd ../zkevm-bridge-service
+cd -
+
+cd ../zkevm-git/zkevm-bridge-service
 make build-docker
 
-cd ../zkevm-bridge-ui
+cd -
+
+cd ../zkevm-git/zkevm-bridge-ui
 docker build . -t zkevm-bridge-ui:local
 
-
+cd -
--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@
 docker-compose up -d zkevm-state-db
 docker-compose up -d zkevm-pool-db
 docker-compose up -d zkevm-event-db
+docker-compose up -d zkevm-bridge-db
 docker-compose up -d zkevm-mock-l1-network
 sleep 1
 docker-compose up -d zkevm-prover
