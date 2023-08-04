38731ba9b5539cbd5c95d135ee63839a119e8192
jianguo <zjg555543@163.com>
2023-08-04 15:17:55 +0800

    update

--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
 cd ../
 git clone -b release/v0.2.1 https://github.com/0xPolygonHermez/zkevm-node.git
 git clone -b release/v0.2.0 https://github.com/0xPolygonHermez/zkevm-bridge-service.git
-git clone -b develop https://github.com/0xPolygonHermez/zkevm-bridge-ui.git
 
 cd ./zkevm-node
 make build-docker
@@ -9,9 +8,5 @@ cd -
 
 cd ./zkevm-bridge-service
 make build-docker
-cd -
-
-cd ./zkevm-bridge-ui
-docker build . -t zkevm-bridge-ui:local
 cd -
 cd zk-demo
--- run.sh
+++ run.sh
@@ -3,8 +3,11 @@ docker-compose up -d zkevm-state-db
 docker-compose up -d zkevm-pool-db
 docker-compose up -d zkevm-event-db
 docker-compose up -d zkevm-bridge-db
+
+sleep 3
+
 docker-compose up -d zkevm-mock-l1-network
-sleep 1
+sleep 3
 docker-compose up -d zkevm-prover
 
 # error, ignore
@@ -12,7 +15,7 @@ docker-compose up -d zkevm-prover
 sleep 3
 docker-compose up -d zkevm-sync
 
-sleep 2
+sleep 3
 docker-compose up -d zkevm-eth-tx-manager
 docker-compose up -d zkevm-sequencer
 docker-compose up -d zkevm-sequence-sender
@@ -20,7 +23,7 @@ docker-compose up -d zkevm-l2gaspricer
 docker-compose up -d zkevm-aggregator
 docker-compose up -d zkevm-json-rpc
 
-
+sleep 3
 docker-compose up -d zkevm-bridge-ui
 docker-compose up -d zkevm-explorer-json-rpc
 docker-compose up -d zkevm-explorer-l1
