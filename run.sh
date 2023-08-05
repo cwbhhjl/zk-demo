589ad2b93a31f005f5e98d3585de6924a6b18a81
zhujianguo <zjg555543@163.com>
2023-08-05 15:27:48 +0800

    fix

--- run.sh
+++ run.sh
@@ -1,6 +1,12 @@
 docker-compose up -d zkevm-state-db
+sleep 3
+
 docker-compose up -d zkevm-pool-db
+sleep 3
+
 docker-compose up -d zkevm-event-db
+sleep 3
+
 docker-compose up -d zkevm-bridge-db
 sleep 3
 
@@ -8,24 +14,37 @@ docker-compose up -d zkevm-mock-l1-network
 sleep 3
 
 docker-compose up -d zkevm-prover
+sleep 3
+
 # error, ignore
-# docker-compose up -d zkevm-approve 
+docker-compose up -d zkevm-approve 
 sleep 3
 
 docker-compose up -d zkevm-sync
 sleep 3
 
 docker-compose up -d zkevm-eth-tx-manager
+sleep 3
+
 docker-compose up -d zkevm-sequencer
+sleep 3
 docker-compose up -d zkevm-sequence-sender
+sleep 3
 docker-compose up -d zkevm-l2gaspricer
+sleep 3
 docker-compose up -d zkevm-aggregator
+sleep 3
 docker-compose up -d zkevm-json-rpc
 sleep 3
 
-docker-compose up -d zkevm-bridge-ui
 docker-compose up -d zkevm-explorer-json-rpc
 docker-compose up -d zkevm-explorer-l1
 docker-compose up -d zkevm-explorer-l2
-sleep 3
+sleep 10
 docker-compose up -d zkevm-bridge-service
+
+
+sleep 5
+docker-compose up -d zkevm-bridge-ui
+
+docker ps -a
